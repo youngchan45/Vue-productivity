d5cf9ccb97ccbc2f209829a11755b46f25aaeffa
youngchan45 <youngchan93@163.com>
2020-03-11 09:41:07 +0800

    整理注释

--- demo/src/components/Systemmanagement/Menumanagement.vue
+++ demo/src/components/Systemmanagement/Menumanagement.vue
@@ -104,15 +104,14 @@ export default {
     },
     moveMenu(moveNode, inNode, type) {
       console.log("所拖对象的ID", moveNode);
-       console.log("移入对象的ID", inNode);
-       console.log("type", type);
-
+      console.log("移入对象的ID", inNode);
+      console.log("type", type);
       let moveNodeLength = moveNode.data.sort.toString().length;
       let inNodeLength = inNode.data.sort.toString().length;
       let moveNodeOne = moveNode.data.sort.toString().substring(0, 1);
       let inNodeOne = inNode.data.sort.toString().substring(0, 1);
-          console.log("----",moveNodeOne,inNodeOne)
-      
+      console.log("----", moveNodeOne, inNodeOne);
+
       if (moveNodeLength === inNodeLength) {
         if (moveNodeOne !== inNodeOne) {
           return type === "prev";
@@ -121,85 +120,18 @@ export default {
         // 不同级进行处理
       }
 
-      //自己的子级只能在内部移动\
-      // if (moveNodeOne === inNodeOne) {
-      //     return type === "prev";
-      //   }else{
-      //     //Dictionarymanagement.vuereturn \
-      //     return false;
-      //   }
+      //自己的子级只能在内部移动
       if (moveNodeLength === inNodeLength) {
         if (moveNodeOne === inNodeOne) {
           return type === "prev";
-        }else{
-        return ;
-      }
+        } else {
+          return;
+        }
       } else {
         // 不同级进行处理
       }
-
-      //子级不能去别人的子级
     },
-    // moveMenu(moveNode, inNode, type) {
-    //   console.log("所拖对象的ID", moveNode.data.sort);
-    //   console.log("移入对象的ID", inNode.data.sort);
-    //   console.log("sort长度", moveNode.data.sort.toString().length);
-    //   console.log(
-    //     "截取移动ID第一位",
-    //     moveNode.data.sort.toString().substring(0, 1)
-    //   );
-    //   console.log(
-    //     "截取插入ID第一位",
-    //     inNode.data.sort.toString().substring(0, 1)
-    //   );
-    //   let moveNodeLength = moveNode.data.sort.toString().length;
-    //   let inNodeLength = inNode.data.sort.toString().length;
-    //   let moveNodeOne = moveNode.data.sort.toString().substring(0, 1);
-    //   let inNodeOne = inNode.data.sort.toString().substring(0, 1);
-    //   //应该先判断移动的节点是父级还是子级！
-    //   // 1. 父级只能在一级移动 （ID.leng=1）{按照顺序改变自己和子级ID}
-    //   // if (moveNodeLength === 1 && inNodeLength === 1) {
-    //   //   // 返回true可拖曳
-    //   //   return type !== "inner";
-    //   // } else {
-    //   //   this.$message.error("只能同级");
-    //   // }
-    //   if (moveNodeLength === 1) {
-    //     // 1.移入的是别人的父级
-    //     if (inNodeLength === 1) {
-    //       return type !== "inner";
-    //     } else {
-    //       // 2.移入的是别人的子级
-    //       //  this.$message.error("只能同级");
-    //       // return
-    //     }
-    //   }
-    //   // 2. 子级只能在二级且在自己的父级中移动 （ID.length=2&&邻近子级ID和自己的ID的第一个数字相同）{按照顺序改变自己ID}
-    //   //先让他不能出去，再让他只能在里面移动
-    //   // if (moveNodeOne === inNodeOne) {
-    //   //   if (moveNodeLength === 2 && inNodeLength === 2) {
-    //   //     return type !== "inner";
-    //   //     // } else {
-    //   //     //   this.$message.error("不可跨级");
-    //   //     //   }
-    //   //     // }else{
-    //   //     //   this.$message.error("不可跨级");
-    //   //     // }
-    //   //   }
-    //   // } else {
-    //   //   this.$message.error("不可跨级");
-    //   // }
-    //   if (moveNodeLength === 2) {
-    //     // 1.移入的是别人的父级和子级
-    //     if (moveNodeOne !== inNodeOne) {
-    //       // this.$message.error("不可跨级");
-    //       // return type!=="next"&&"inner"&&"prev"
-    //     }else if(inNodeLength === 1&&moveNodeOne === inNodeOne){
-    //       // 2.移入的是自己的父级
-    //       // return type == "inner"
-    //     }
-    //   }
-    // },
+
     delMenu() {},
     resetMenu() {},
     saveMenu() {}
